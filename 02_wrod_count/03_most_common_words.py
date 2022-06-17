b4d2412faeea5724eed4fda795b31c9a5f72c9a6
bartlomiejbondyra <134393@student.pwr.edu.pl>
2022-06-17 08:40:39 +0200

    word count

--- 02_wrod_count/03_most_common_words.py
+++ 02_wrod_count/03_most_common_words.py
@@ -6,13 +6,27 @@ WORD_RE = re.compile(r'[\w]+')
 
 class MRMostCommonWord(MRJob):
 
-    def mapper(self, _, line):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_words,
+                   reducer=self.reducer_count_words),
+            MRStep(mapper=self.mapper_get_keys,
+                   reducer=self.reducer_finf_most_common_word)
+        ]
+
+    def mapper_get_words(self, _, line):
         words = WORD_RE.findall(line)
         for word in words:
             yield word.lower(), 1
 
-    def reducer(self, word, counts):
+    def reducer_count_words(self, word, counts):
         yield word, sum(counts)
+
+    def mapper_get_keys(self, key, value):
+        yield None, (value, key)
+
+    def reducer_finf_most_common_word(self, key, values):
+        yield max(values)
 
 if __name__ == '__main__':
     MRMostCommonWord.run()
