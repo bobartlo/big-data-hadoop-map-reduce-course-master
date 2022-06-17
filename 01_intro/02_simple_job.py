e7765572c4545fb57a1f1fe46ca0140af26505bc
bartlomiejbondyra <134393@student.pwr.edu.pl>
2022-06-17 07:28:57 +0200

    simple mrjob

--- 01_intro/02_simple_job.py
+++ 01_intro/02_simple_job.py
@@ -2,8 +2,10 @@ from mrjob.job import MRJob
 
 class MRSimpleJob(MRJob):
 
-    def mapper(selfself, _, value):
+    def mapper(selfself, _, line):
         yield 'line', 1
+        yield 'words', len(line.split())
+        yield 'chars', len(line)
 
     def reducer(self, key, values):
         yield key, sum(values)
